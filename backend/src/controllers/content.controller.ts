ae2726ffea681c2fc65ab3d50a7e6ec2687a56a4
sakshi2005-30 <sakshikurda305@gmail.com>
2026-08-22 17:44:54 +0530

    Added delete content

--- backend/src/controllers/content.controller.ts
+++ backend/src/controllers/content.controller.ts
@@ -72,3 +72,31 @@ export const getContent=async(req:Request,res:Response):Promise<void>=>{
         })
     }
 }
+
+export const deleteContent=async(req:Request,res:Response):Promise<void>=>{
+    try{
+        const userId=req.userId;
+        const {contentId}=req.params;
+        if(!userId){
+            res.status(401).json({message:"Unauthorized user"});
+            return;
+        }
+        if(!contentId){
+            res.status(400).json({message:"Content id is needed"});
+            return;
+        }
+        const deleteContent=await Content.findOneAndDelete({_id:contentId,userId}).exec();
+        if(!deleteContent){
+            res.status(404).json({message:"Content not found or you don't have permission to delete it"});
+            return;
+        }
+        res.status(200).json({
+            message:"Content deleted successfully"
+        })
+        
+    }
+    catch(error){
+        console.log("Error in deleting content",error);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
--- backend/src/routes/content.routes.ts
+++ backend/src/routes/content.routes.ts
@@ -1,7 +1,8 @@
 import {Router} from "express"
-import {createContent,getContent} from "../controllers/content.controller.js"
+import {createContent,getContent,deleteContent} from "../controllers/content.controller.js"
 import {userMiddleware} from "../middlewares/user.middleware.js"
 const router=Router();
 router.post("/",userMiddleware,createContent);
-router.get("/",userMiddleware,getContent)
+router.get("/",userMiddleware,getContent);
+router.delete("/:contentId",userMiddleware,deleteContent);
 export default router;
