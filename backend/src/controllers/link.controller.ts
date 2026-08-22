862090457fc20ccb5010e50eaab202aae832487f
sakshi2005-30 <sakshikurda305@gmail.com>
2026-08-22 20:34:34 +0530

    slight chnage in share brain controller,added getpublic brain contoller

--- backend/src/controllers/link.controller.ts
+++ backend/src/controllers/link.controller.ts
@@ -2,6 +2,7 @@ import { Link } from "../models/link.model.js";
 import { Content } from "../models/content.model.js";
 import type { Request,Response } from "express";
 import crypto from "crypto"
+import { User } from "../models/user.model.js";
 const generateHash=(length=10):string=>{
     const h=crypto.randomBytes(length).toString("hex").slice(0,10);
     return h;
@@ -13,7 +14,7 @@ export const shareBrain=async(req:Request,res:Response):Promise<void>=>{
             res.status(401).json({message:"Unauthorized user"});
             return;
         }
-        const {share}=req.query;
+        const {share}=req.body;
         if(share){
             const link=await Link.findOne({userId});
             if(link){
@@ -39,6 +40,35 @@ export const shareBrain=async(req:Request,res:Response):Promise<void>=>{
     }
     catch(error){
         console.log("Error in generating sharelink",error);
+        res.status(500).json({
+            message:"Internal server error"
+        }) 
+    }
+}
+export const getPublicBrain=async(req:Request,res:Response):Promise<void>=>{
+    try{
+       const {sharelink}=req.params;
+       if(!sharelink){
+        res.status(400).json({
+            message:"sharelink is required "
+        })
+        return;
+       }
+       const link=await Link.findOne({hash:sharelink});
+       if(!link){
+        res.status(404).json({message:"Invalid sharelink"});
+        return;
+       }
+       const user=await User.findOne({_id:link.userId}).select("username email").exec();
+       const content=await Content.find({userId:link.userId}).exec();
+       res.status(200).json({
+        message:"Public share content fetched sucessfully",
+        user,
+        content:content
+       })
+    }
+    catch(error){
+        console.log("Error in public share brain",error);
         res.status(500).json({
             message:"Internal server error"
         }) 
--- backend/src/routes/link.routes.ts
+++ backend/src/routes/link.routes.ts
@@ -1,6 +1,7 @@
-import {shareBrain} from "../controllers/link.controller.js"
+import {shareBrain,getPublicBrain} from "../controllers/link.controller.js"
 import {Router} from "express"
 import {userMiddleware} from "../middlewares/user.middleware.js"
 const router=Router()
 router.post("/share",userMiddleware,shareBrain);
+router.get("/:sharelink",getPublicBrain);
 export default router;
