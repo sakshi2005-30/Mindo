d5d17f4c17136b455449799a7cad6455c258e2f9
sakshi2005-30 <sakshikurda305@gmail.com>
2026-09-05 23:42:30 +0530

    fix:removed unnessecary imports

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+
 import { Navbar } from "../components/Navbar";
 import { Sidebar } from "../components/Sidebar";
 import { ContentFeed } from "./ContentFeed";
